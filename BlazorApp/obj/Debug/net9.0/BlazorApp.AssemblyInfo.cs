b54d2f488320431caa2eabb28ab65d1d9cd9e5f1
pedram <pedramalizade03@gmail.com>
2025-10-09 18:46:23 +0330

    Add Some Product

--- BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f101d994ba887998b6f71d98e1c5e862ccbcf0bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27c0c577a46bde432b1e1608ac1801c2fd692dc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
