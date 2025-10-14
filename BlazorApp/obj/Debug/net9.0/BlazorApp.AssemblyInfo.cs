3b4fd00ade7c87158aefde1c9d6bb0bc311ebc77
pedram <pedramalizade03@gmail.com>
2025-10-14 17:28:07 +0330

    Creating a nested component

--- BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48ad98b452e658b6c549771dddeeef06a1aabe76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852beb888b4f54477a2a287b00cd0eb9d1312b6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
